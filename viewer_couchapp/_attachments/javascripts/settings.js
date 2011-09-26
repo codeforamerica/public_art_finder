ca42206c5ff1142539989062edfcef2e5b91c2eb
John Mertens <john.mertens@gmail.com>
2011-09-26 14:17:35 -0700

    Made settings.js more abstract and then removed it from .gitignore

--- viewer_couchapp/_attachments/javascripts/settings.js
+++ viewer_couchapp/_attachments/javascripts/settings.js
@@ -1,5 +1,5 @@
 var Muralapp = {
-    google_analytics : 'UA-XXXXXXXX-X',
+    google_analytics : 'UA-23026072-1',
     db : {
         path : location.protocol+'//'+location.host+'/'+location.pathname.split('/')[1]
     }
