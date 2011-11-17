ce66f89246ebfa431a511da9aabc53ffdf81b4ca
John Mertens <john.mertens@gmail.com>
2011-11-17 00:51:38 -0800

    Foursquare auth working, ready to load data.

--- foursquare_uploader/upload.rb
+++ foursquare_uploader/upload.rb
@@ -1,40 +1,50 @@
-require 'rubygems'
-require 'foursquare'
+begin
+  require 'sinatra'
+  require 'omniauth'
+  require 'omniauth-foursquare'
+  require 'foursquare2'
+  require 'json'
+  require 'curb'
+rescue LoadError
+  require 'rubygems'
+  retry
+end
 
+use Rack::Session::Cookie
+use OmniAuth::Builder do
+ provider :foursquare, ENV['FSQ_CLIENT_ID'], ENV['FSQ_CLIENT_SECRET']
+end
+
+set :port, 3000
+enable :sessions
 oauth_key = ENV['FSQ_CLIENT_ID']
 oauth_secret = ENV['FSQ_CLIENT_SECRET']
+couchview = 'http://127.0.0.1:5984/public_art_sf/_design/process/_view/foursquare'
 
-oauth = Foursquare::OAuth.new(oauth_key, oauth_secret)
+get '/' do
+  <<-HTML
+    <a href='/auth/foursquare'>Sign in to foursquare</a>
+  HTML
+end
 
-request_token = oauth.request_token.token
-request_secret = oauth.request_token.secret
+get '/auth/:name/callback' do
+  auth = request.env['omniauth.auth']
 
-# redirecting user to foursquare to authorize
-oauth.request_token.authorize_url
-# 
-# # foursquare redirects back to your callback url, passing the verifier in the url params
-# 
-# access_token, access_secret = oauth.authorize_from_request(request_token, request_secret, verifier)
-# 
-# # save the user's access token and secret
-# 
-# 
-# oauth = Foursquare::OAuth.new(oauth_key, oauth_secret)
-# oauth.authorize_from_access(access_token, access_secret)
-# foursquare = Foursquare::Base.new(oauth)
-# 
-# foursquare.test
-# 
-# foursquare.venues :geolat => geolat, :geolong => geolong, :limit => 10, :q => 'pizza'
-# foursquare.tips :geolat => geolat, :geolong => geolong, :limit => 10
-# foursquare.checkins :geolat => geolat, :geolong => geolong
-# 
-# checkin = {
-#   :vid => vid,
-#   :shout => "this is what i'm up to",
-#   :venue => "Cohabitat",
-#   :private => 0,
-#   :twitter => 0,
-#   :geolat => geolat,
-#   :geolong => geolong
-# }
+  token = auth[:credentials][:token]
+  puts token
+  # Create the client
+  client = Foursquare2::Client.new({:oauth_token => token})
+
+  # get the list of pieces
+  the_couch = Curl::Easy.http_get(couchview)
+
+  resp = JSON.parse(the_couch.body_str)
+  rows = resp['rows']
+  allart = rows.map {|art| art['value'] }
+  puts allart[0]
+  client.add_venue(allart[0])
+
+  # allart.each { |piece| puts piece }
+  # Hit the API with whatever we want
+
+end
